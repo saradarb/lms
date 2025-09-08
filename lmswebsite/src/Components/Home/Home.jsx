62f5984d0df2d75a614b5bd52bf169b96feb610b
Sara <saradernouni@gmail.com>
2025-09-08 10:46:45 -0400

    Update Home.jsx

--- lmswebsite/src/Components/Home/Home.jsx
+++ lmswebsite/src/Components/Home/Home.jsx
@@ -122,28 +122,7 @@ const Home = () => {
 
   return (
     <div className="home-container22">
-      {/* Navigation Bar */}
-      <nav className="navbar22">
-        {/* Logo */}
-        <div className="logo-section22">
-          <div className="logo-icon22">
-            📖
-          </div>
-          <span className="logo-text22">Learnify</span>
-        </div>
-
-        {/* Center Navigation */}
-        <div className="nav-center22">
-        <Link to="/" className="nav-button22" style={{ textDecoration: "none" }} >Home</Link>
-        <Link to="/AllCourses" className="nav-button22 active" style={{ textDecoration: "none" }} >Courses</Link>
-        </div>
-
-        {/* Right Side Buttons */}
-        <div className="auth-buttons22">
-        <Link to="/Login" className="login-btn22" style={{ textDecoration: "none" }} >Login</Link>
-        <Link to="/SignUp" className="signup-btn22" style={{ textDecoration: "none" }} >Sign Up</Link>
-        </div>
-      </nav>
+     <NavBar />
 
       {/* Vanta Globe Section */}
       <div className="vanta-section22">
